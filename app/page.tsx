6c5f468d8cb43a644313bbf0efb5c5c62cbeb846
loveheenavasu <trivediheenalove@gmail.com>
2024-05-15 18:39:19 +0530

    integrated all the tmdb api's

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import fetchAllMovies from "./actions/BaseUrl";
 export default async function Home() {
-// const data = await fetchAllMovies()
-// console.log('sdfdg', data)
   return (
     <main>
       Welcome to Movie App
